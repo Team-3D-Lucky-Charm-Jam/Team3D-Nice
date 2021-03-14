46a607b9488a0866afca3f651386c85438eac094
Bryan Ambriz <beambriz2017@gmail.com>
2021-03-14 14:05:35 -0700

    Intro Scene and credits

---
+++ Assets/Scripts/IntroSceneScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroSceneScript : MonoBehaviour
+{
+    public AudioSource aSource;
+    public AudioClip ac;
+    public string nextScene;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource.PlayOneShot(ac);
+        time = ac.length;
+        Invoke("NextScene", time); 
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+   
+}
