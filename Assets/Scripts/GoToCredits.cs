1360e229253382ad8cbf5fffa66ff4fe7ae9cc4f
Bryan Ambriz <beambriz2017@gmail.com>
2021-03-14 13:31:21 -0700

    Added new character rpefab and scene manager

---
+++ Assets/Scripts/GoToCredits.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToCredits : MonoBehaviour
+{
+    public string nextScene;
+   public void LoadScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
